b407f1f8799f3e796373c5bf3940e59d07dc9a75
Damian Gola <gola@imbs.uni-luebeck.de>
2015-07-13 17:10:42 +0200

    weights are feature specific not individual specific

--- R/resample.R
+++ R/resample.R
@@ -113,7 +113,7 @@ doResampleIteration = function(learner, task, rin, i, measures, weights, model, 
 
   err.msgs = c(NA_character_, NA_character_)
   messagef("Training...")
-  m = train(learner, task, subset = train.i, weights = weights[train.i])
+  m = train(learner, task, subset = train.i, weights = weights)
   messagef("Training done.")
   if (isFailureModel(m))
     err.msgs[1L] = getFailureModelMsg(m)
