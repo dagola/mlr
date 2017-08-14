693b5cf29df836f2395004cecef05d2bff599792
Damian Gola <gola@imbs.uni-luebeck.de>
2017-08-14 16:11:10 +0200

    add riskScoreR learner

---
+++ R/RLearner_classif_riskScoreR.R
@@ -0,0 +1,34 @@
+
+#' @export
+makeRLearner.classif.riskScorer = function() {
+  makeRLearnerClassif(
+    cl = "classif.riskScorer",
+    package = "riskScoreR",
+    par.set = makeParamSet(
+      makeLogicalLearnerParam(id = "weight", default = TRUE),
+      makeUntypedLearnerParam(id = "beta", default = TRUE),
+      makeNumericVectorLearnerParam(id = "importance", tunable = FALSE)
+    ),
+    properties = c("twoclass", "numerics", "prob"),
+    name = "Risk allele scoring",
+    short.name = "riskscorer",
+    note = ""
+  )
+}
+
+#' @export
+trainLearner.classif.riskScorer <- function(.learner, .task, .subset, .weights, ...) {
+  riskScorer(y.name = getTaskTargetNames(.task),
+             data = getTaskData(.task, .subset),
+             feature.names = getTaskFeatureNames(.task), ...)
+}
+
+#' @export
+predictLearner.classif.riskScorer <- function(.learner, .model, .newdata, ...) {
+  p <- predict(riskScorer = .model$learner.model, newdata = .newdata, type = .learner$predict.type)
+  if(.learner$predict.type == "response") {
+    return(p[[1]])
+  } else {
+    return(as.matrix(p))
+  }
+}
